70e24ff3a41dcb08defed08317dc02a736f9d57d
Dinh Quang Trung <trungdq88@gmail.com>
2014-02-14 14:46:35 -0800

    Basic feature is completed

---
+++ FormulasTextBox.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Assignment4
+{
+    public class FormulasTextBox : TextBox
+    {
+        
+
+        /******************************************************/
+        /*  Private fields 
+        /******************************************************/
+        private Boolean _numericOnly;
+        private Color _invalidFormulasColor = Color.Red;
+        private Color _validFormulasColor = Color.Blue;
+        private Color _inputFormulasColor = Color.White;
+        private Boolean _cancelKeyDueToShortcuts;
+
+        private Boolean IsFormulasValid { get; set; }
+
+        /******************************************************/
+        /*  Properties and events 
+         *  See more:
+         *  http://stackoverflow.com/a/2081964/1420186
+         *  http://msdn.microsoft.com/en-us/library/system.componentmodel.categoryattribute.aspx
+        /******************************************************/
+        [Description("Active Numeric Only"), Category("Numeric Only")]
+        public Boolean NumericOnly
+        {
+            get { return _numericOnly; }
+            set { _numericOnly = value; }
+        }
+
+        [Description("Invalid Formulas Color"), Category("Numeric Only")]
+        public Color InvalidFormulasColor
+        {
+            get { return _invalidFormulasColor; }
+            set { _invalidFormulasColor = value; }
+        }
+
+        [Description("Valid Formulas Color"), Category("Numeric Only")]
+        public Color ValidFormulasColor
+        {
+            get { return _validFormulasColor; }
+            set { _validFormulasColor = value; }
+        }
+
+        [Description("Input Formulas Color"), Category("Numeric Only")]
+        public Color InputFormulasColor
+        {
+            get { return _inputFormulasColor; }
+            set { _inputFormulasColor = value; }
+        }
+
+
+        /******************************************************/
+        /** Override methods
+        /******************************************************/
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+            // Shortcut keys to improve input speed. Make it pluggable in the future
+            switch (e.KeyCode)
+            {
+                case Keys.T:
+                    _cancelKeyDueToShortcuts = true;
+                    AppendText("00");
+                    break;
+                case Keys.N:
+                    _cancelKeyDueToShortcuts = true;
+                    AppendText("000");
+                    break;
+                default:
+                    _cancelKeyDueToShortcuts = false;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Decide if the pressed key is need to appended to the text box
+        /// http://msdn.microsoft.com/en-us/library/vstudio/system.windows.forms.control.keydown(v=vs.100).aspx
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+            if (_cancelKeyDueToShortcuts)
+            {
+                e.Handled = true;
+            }
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            // When user is typing the input, use default color;
+            BackColor = _inputFormulasColor;
+        }
+        
+        /// <summary>
+        /// Override OnLeave method to validate formulas when the control lost focus
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnLeave(EventArgs e)
+        {
+            base.OnLeave(e);
+            IsFormulasValid = true;
+            if (Text.Length > 0)
+            {
+                if (Text[0] == '=')
+                {
+                    // User is going to enter a formulas
+
+                    String input = Text.TrimStart('=');
+                    try
+                    {
+                        Double result = EvaluateExpression(input);
+                        Text = result + "";
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Expression is invalid");
+                    }
+
+                } else
+                {
+                    // User is going to enter a value
+                    try
+                    {
+                        Double result = Convert.ToDouble(Text);
+                    }
+                    catch (Exception)
+                    {
+                        // Input is invalid
+                        IsFormulasValid = false;
+                        MessageBox.Show("Invalid input value!");
+                    }
+                }
+                
+            } else
+            {
+                Text = "0";
+            }
+
+            // Update back color
+            if (IsFormulasValid)
+            {
+                BackColor = _validFormulasColor;
+            } else
+            {
+                BackColor = _invalidFormulasColor;
+                Focus();
+            }
+        }
+
+        /// <summary>
+        /// There are many ways to implement this method
+        /// As an ACM student, they would prefer the Binary Expression Tree Aglorithm:
+        ///     http://en.wikipedia.org/wiki/Binary_expression_tree
+        /// But ain't body got time for that, I am not relaly good at Aglorithm, so I
+        /// found a nice-tricky solution from Teoman Soygul in StackoverFlow
+        ///     http://stackoverflow.com/a/6052679/1420186
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        private double EvaluateExpression(String expression)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+                table.Columns.Add("expression", string.Empty.GetType(), expression);
+                DataRow row = table.NewRow();
+                table.Rows.Add(row);
+                return double.Parse((string)row["expression"]);  
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
---
+++ Main.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Assignment4
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numberTextBox1 = new Assignment4.FormulasTextBox();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSubmit.Location = new System.Drawing.Point(589, 12);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(85, 80);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Press TAB or click Submit to complete input";
+            // 
+            // numberTextBox1
+            // 
+            this.numberTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numberTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberTextBox1.InputFormulasColor = System.Drawing.Color.White;
+            this.numberTextBox1.InvalidFormulasColor = System.Drawing.Color.Red;
+            this.numberTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.numberTextBox1.Name = "numberTextBox1";
+            this.numberTextBox1.NumericOnly = false;
+            this.numberTextBox1.Size = new System.Drawing.Size(571, 80);
+            this.numberTextBox1.TabIndex = 4;
+            this.numberTextBox1.ValidFormulasColor = System.Drawing.Color.Blue;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 115);
+            this.Controls.Add(this.numberTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Assignment 4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label1;
+        private FormulasTextBox numberTextBox1;
+    }
+}
